18b74a5e985ae5e30b01db1950d2455d0ffa6b92
Rohit-S-Singh <rohitshekrsingh@gmail.com>
2022-07-07 17:45:42 +0530

    changes

--- server.js
+++ server.js
@@ -1,22 +1,55 @@
 const express = require("express");
+const reader = require('xlsx')
 const app = express();
+
+var cors = require("cors");
+
+app.use(cors());
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
 
 const { PythonShell } = require("python-shell");
 
-app.get("/", (req, res, next) => {
-  let options = {
-    mode: "text",
-    pythonOptions: ["-u"],
-    scriptPath: "",
-    args: ["shubhamk314"],
-  };
+app.post("/", (req, res) => {
 
-  PythonShell.run("./check.py", options, function (err, result) {
-    if (err) throw err;
-    // console.log("result: ", result.toString());
-    res.send("abc");
-  });
+const file = reader.readFile('./test.xlsx')
+
+var a = req.body;
+  
+// Sample data set
+let student_data = [];
+
+var ob = {};
+
+Object.keys(a).map((aa)=>{
+  ob[aa] = a[aa];
+})
+
+student_data.push(ob);
+
+console.log("coming");
+  console.log(req.body);
+
+
+  res.status(200).json({message:"helloworld"});
+
+  // PythonShell.run("./test.py", options, function (err, result) {
+  //   if (err) throw err;
+  //   console.log("result: ", result.toString());
+  //   res.send("abc");
+  // });
 });
 
 const port = 8000;
 app.listen(port, () => console.log(`Server connected to ${port}`));
+
+
+// const ws = reader.utils.json_to_sheet(student_data)
+  
+// reader.utils.book_append_sheet(file,ws,"Sheet3")
+  
+// Writing to our file
+// reader.writeFile(file,'./test.xlsx')
