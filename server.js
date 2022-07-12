3da86b4130d5a80860f699129d3289307b22155d
Rohit-S-Singh <rohitshekrsingh@gmail.com>
2022-07-12 16:23:16 +0530

    database config and signup feature

--- server.js
+++ server.js
@@ -1,26 +1,63 @@
 const express = require("express");
 const reader = require('xlsx')
 const app = express();
+const chalk = require("chalk");
+
+const routes = require("./server/index");
+
+const mongoose = require("./server/models/config");
 
 var cors = require("cors");
 
 app.use(cors());
 
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+app.use(routes);
 
 const { PythonShell } = require("python-shell");
 
 app.post("/", async (req, res) => {
 
+    const file = reader.readFile('./test.xlsx')
+
+    var a = req.body;
+  
+    // Sample data set
     
+    let student_data = [];
+
+    var ob = {};
+
+    Object.keys(a).map((aa)=>{
+         ob[aa] = a[aa];
+    })
+
+    student_data.push(ob);
+
     console.log("coming");
     console.log(req.body);
 
+
+
+    var options = {
+        mode: 'text',
+        pythonPath: 'python',
+        pythonOptions: ['-u'],
+        scriptPath: '',
+        args: ['Ritvik', '8', 'value3']
+    };
+        
     
-    //     const spawn = require("child_process").spawn;
+    await PythonShell.run("./test.py", options, function (err, result) {
+        if (err) throw err;
+        console.log("result: ", result);
+        res.send("abc");
+    });
+
+//     const spawn = require("child_process").spawn;
 //     const pythonProcess = exec('python3',["./test.py", 2, 4]);
 
 //         pythonProcess.stdout.on('data', function(data) {
@@ -33,44 +70,7 @@ app.post("/", async (req, res) => {
 
 });
 
-// const port = 8000;
-app.listen(8000, () => console.log(`Server connected to ${port}`));
-
-
-// const ws = reader.utils.json_to_sheet(student_data)
-  
-// reader.utils.book_append_sheet(file,ws,"Sheet3")
-  
-// Writing to our file
-
-// const file = reader.readFile('./test.xlsx')
-
-// var a = req.body;
-
-// // Sample data set
-
-// let student_data = [];
-
-// var ob = {};
-
-// Object.keys(a).map((aa)=>{
-//      ob[aa] = a[aa];
-// })
-
-// student_data.push(ob);
-
-    // var options = {
-    //     mode: 'text',
-    //     pythonPath: 'python',
-    //     pythonOptions: ['-u'],
-    //     scriptPath: '',
-    //     args: ['Ritvik', '8']
-    // };
-        
-    
-    // await PythonShell.run("./test.py", options, function (err, result) {
-    //     if (err) throw err;
-    //     console.log("result: ", result);
-    //     res.send("abc");
-    // });
-// reader.writeFile(file,'./test.xlsx')
+const port = 8000;
+app.listen(port, () => console.log(`${chalk.green("✓")} ${chalk.magenta(
+      `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+    )}`));
