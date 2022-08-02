7460408e23a14a18a5d8f87eb0da8f4047495a08
Rohit-S-Singh <rohitshekrsingh@gmail.com>
2022-08-02 22:58:46 +0530

    authentication

--- server/models/config.js
+++ server/models/config.js
@@ -12,3 +12,6 @@ mongoose
     console.log(`${chalk.green("✓")} ${chalk.blue("MongoDB Connected!")}`)
   )
   .catch((err) => console.log(err));
+
+
+  // mongodb+srv://pjain1206:seeyou12345@cluster0.jwuxxx1.mongodb.net/?retryWrites=true&w=majority  -> for the cloud one
