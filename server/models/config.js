c42984e7ffe1408510408e1cee589df55a3ea330
payal1206 <68314999+payal1206@users.noreply.github.com>
2022-08-02 23:02:14 +0530

    project setup

--- server/models/config.js
+++ server/models/config.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 // mongoose.set("useCreateIndex", true);
 mongoose
-  .connect("mongodb://localhost:27017", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-  })
+  .connect(
+    "mongodb+srv://pjain1206:seeyou12345@cluster0.jwuxxx1.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: false,
+    }
+  )
   .then(() =>
     console.log(`${chalk.green("✓")} ${chalk.blue("MongoDB Connected!")}`)
   )
