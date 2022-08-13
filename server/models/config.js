69c16ac082064e2fee8445ad254a459449168c17
Rohit-S-Singh <rohitshekrsingh@gmail.com>
2022-08-14 02:12:32 +0530

    saving movies

--- server/models/config.js
+++ server/models/config.js
@@ -3,18 +3,14 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 // mongoose.set("useCreateIndex", true);
 mongoose
-  .connect(
-    "mongodb+srv://pjain1206:seeyou12345@cluster0.jwuxxx1.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useFindAndModify: false,
-    }
-  )
+  .connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
   .then(() =>
     console.log(`${chalk.green("✓")} ${chalk.blue("MongoDB Connected!")}`)
   )
   .catch((err) => console.log(err));
 
-
-  // mongodb+srv://pjain1206:seeyou12345@cluster0.jwuxxx1.mongodb.net/?retryWrites=true&w=majority  -> for the cloud one
+// mongodb+srv://pjain1206:seeyou12345@cluster0.jwuxxx1.mongodb.net/?retryWrites=true&w=majority  -> for the cloud one
