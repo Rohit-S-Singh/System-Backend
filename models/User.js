c8c46012c8d53a0a37f375bc36112bcbfd893876
rohitssingh17 <rohitshekrsingh@gmail.com>
2025-07-10 22:04:20 +0530

    Email tmplate

--- models/User.js
+++ models/User.js
@@ -1,37 +1,49 @@
 import mongoose from "mongoose";
 
-
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String
-    },
-    email: {
-        type: String,
-        required: [true, "Please provide a email"],
-        unique: true,
-    },
-    password: {
-        type: String,
-    },
-    phone: {
-        type: Number,
-    },
-    online: {
-        type: Boolean,
-        default: false,
-    },
-    accessToken: {
-        type: String,
-    },
-    refreshToken: {
-        type: String,
-    },
-    tokenExpiry: {
-        type: Date,
-    }
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+  online: {
+    type: Boolean,
+    default: false,
+  },
+  accessToken: {
+    type: String,
+  },
+  refreshToken: {
+    type: String,
+  },
+  tokenExpiry: {
+    type: Date,
+  },
 
-},{timestamps : true});
+  // ✅ New field to store HTML email template
+  htmlEmailTemplate: {
+    rawTemplate: {
+      type: String,
+    },
+    placeholders: {
+      type: Object,
+    },
+    finalHtml: {
+      type: String,
+    },
+  },
 
-const User = mongoose.models.users ||  mongoose.model("users", UserSchema);
+}, { timestamps: true });
+
+const User = mongoose.models.users || mongoose.model("users", UserSchema);
 
 export default User;
