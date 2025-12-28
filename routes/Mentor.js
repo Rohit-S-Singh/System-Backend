e32110c075ff4125f8c9fb65591a3d79db3e11a7
Tejas Sangvi <tejassangvi7@gmail.com>
2025-12-28 16:44:01 +0530

    new

--- routes/Mentor.js
+++ routes/Mentor.js
@@ -1,6 +1,11 @@
 import express from 'express';
-import { getMentorDetails } from '../controller/interviews/Interview.js';
+// import { getMentorDetails } from '../controller/interviews/Interview.js';
+import { requestBecomeMentor ,getApprovedActiveMentors,getMentorDetails } from '../controller/Mentor/Mentor.js';
 const Router = express.Router();
 
+
+
+Router.post("/become-mentor", requestBecomeMentor);
+Router.get('/show-all-mentor', getApprovedActiveMentors);
 Router.get('/mentor/:mentorId', getMentorDetails);
 export default Router; 
